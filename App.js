4ef911507750df712228e576a1927ce9ad3fa408
Francis J <fajoaq@gmail.com>
2021-04-08 20:28:34 -0400

    Adde settings button to review screen- connect to navigation

--- App.js
+++ App.js
@@ -2,40 +2,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
 import { StyleSheet } from 'react-native';
-/* import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator as tabNav} from 'react-navigation-tabs';
-import { createStackNavigator as stackNav} from 'react-navigation-stack'; */
-
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MainScreen from './screens/MainScreen'
 
-
-
 export default function App() {
   const WelcomeTab = createBottomTabNavigator();
-
-
-/*   const reviewStack = stackNav({
-    review: ReviewScreen,
-    settings: SettingsScreen
-  });
-
-  const mainTab = createAppContainer(tabNav({
-    map: MapScreen,
-    deck: DeckScreen,
-    reviewFlow: reviewStack
-  }));
-
-  const MainNavigator = createAppContainer(tabNav({
-      welcome: WelcomeScreen,
-      auth: AuthScreen,
-      mainFlow: mainTab
-    })
-  ); */
 
   return (
     <NavigationContainer>
--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
-const ReviewScreen = ({ navigation }) => (
+const ReviewScreen = ({ navigation }) => {
+    useEffect(() => {
+        navigation.setOptions({
+            headerTitle: "Review Jobs",
+            headerRight: () => (
+                <Button 
+                    title="Settings"
+                    onPress={() => navigation.navigate('Settings')}
+                />
+            )
+        })
+    }, []);
+    
+    return (
         <React.Fragment>
             <Text>Review Screen</Text>
             <Text>Review Screen</Text>
             <Text>Review Screen</Text>
             <Text>Review Screen</Text>
         </React.Fragment>
-);
+    );
+};
 
 export default ReviewScreen;
