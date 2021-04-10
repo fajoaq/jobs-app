0f570c14e941cf2d66600c41c7c9bf9d0796d2d8
Francis J <fajoaq@gmail.com>
2021-04-10 02:25:21 -0400

    cleanup slide code

--- components/Slides.js
+++ components/Slides.js
@@ -1,52 +1,43 @@
 import React from 'react';
 import { Platform, StyleSheet, Text, View, ScrollView, Dimensions, Image } from 'react-native';
 
-const DEVICE_WIDTH = Dimensions.get('window').width;
-const DEVICE_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 
 const Slides = ({ data }) => {
 
     return (
         <View style={styles.container}>
-          <ScrollView horizontal={true} pagingEnabled={true} showsHorizontalScrollIndicator={false}>
-              { data.map((slide, index) => (
-                <View key={"slide " + index} style={[styles.slide, slideColor[index].style]}>
-                    <Text style={styles.headerText}>{ slide.text }</Text>
+          <ScrollView 
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+          >
+            { data.map((slide, index) => (
+                <View 
+                  key={"slide " + (index + 1)} 
+                  style={[styles.slide, { backgroundColor: slide.color }]}
+                >
+                  <Text style={styles.headerText}>{ slide.text }</Text>
                 </View>
               ))
-  
-              }
+            }
           </ScrollView>
         </View>
     );
 }
 
-const slideColor = [
-    {style: {
-        backgroundColor: '#F44336'
-    }},
-    {style: {
-        backgroundColor: '#9C27B0'
-    }},
-    {style: {
-        backgroundColor: '#3F51B5',
-    }},
-    {style: {
-        backgroundColor: '#009688',
-    }}
-]
-
 const styles = StyleSheet.create({
   container: {
-    height: DEVICE_HEIGHT,
+    height: SCREEN_HEIGHT,
     marginTop: 20,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#e5e5e5",
   },
   slide:{
-    width: DEVICE_WIDTH,
+    width: SCREEN_WIDTH,
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row'
