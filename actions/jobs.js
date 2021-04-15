36d47384a738f2172d54fc1ec5db9f971de7fc50
Francis J <fajoaq@gmail.com>
2021-04-14 22:44:42 -0400

    Created mapview and started job search api

---
+++ actions/jobs.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import INDEED_API_KEY from '../api/indeed';
+
+export const fetchJobs = (dispatch) => {
+    ( async () => {
+        let something = await axios(); //FINISH THIS
+    })();
+};
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,23 +1,54 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Dimensions, ActivityIndicator } from 'react-native';
+import MapView from 'react-native-maps';
 
 const MapScreen = ({ navigation }) => {
+    const initCoords = {
+        longitude: -122,
+        latitude: 37,
+        longitudeDelta: 0.04,
+        latitudeDelta: 0.09
+    }
+    const [loading, setLoading] = useState(true);
+    const [region, setRegion] = useState(initCoords);
 
     useEffect(() => {
         navigation.setOptions({
             headerTitle: "Maps"
         })
+        setLoading(false);
     }, []);
 
     return (
-        <View>
-            <Text>MapScreen</Text>
-            <Text>MapScreen</Text>
-            <Text>MapScreen</Text>
-            <Text>MapScreen</Text>
-            <Text>MapScreen</Text>
+        <View style={ styles.container}>
+            { loading ? 
+                <ActivityIndicator 
+                    style={ styles.map } 
+                    size="large" 
+                    color="#0000ff" 
+                />
+                : 
+                <MapView 
+                    style={ styles.map } 
+                    region={ region }
+                    onRegionChangeComplete={ (region) => setRegion(region) }
+                /> 
+            }
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+});
 
 export default MapScreen;
